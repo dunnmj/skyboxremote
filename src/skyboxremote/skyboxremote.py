1367d59e2b8d4c001875385b3b0082b067cde0cb
dunnmj <dunnmj@users.noreply.github.com>
2024-09-18 19:42:27 +0000

    fix: Handle OS error in check connectable

--- src/skyboxremote/skyboxremote.py
+++ src/skyboxremote/skyboxremote.py
@@ -18,6 +18,52 @@ class NotASkyBoxError(SkyBoxConnectionError):
     """Device probably is not a Sky box error."""
 
 
+_KEY_MAP = {
+    "power": 0,
+    "select": 1,
+    "backup": 2,
+    "dismiss": 2,
+    "channelup": 6,
+    "channeldown": 7,
+    "interactive": 8,
+    "sidebar": 8,
+    "help": 9,
+    "services": 10,
+    "search": 10,
+    "tvguide": 11,
+    "home": 11,
+    "i": 14,
+    "text": 15,
+    "up": 16,
+    "down": 17,
+    "left": 18,
+    "right": 19,
+    "red": 32,
+    "green": 33,
+    "yellow": 34,
+    "blue": 35,
+    "0": 48,
+    "1": 49,
+    "2": 50,
+    "3": 51,
+    "4": 52,
+    "5": 53,
+    "6": 54,
+    "7": 55,
+    "8": 56,
+    "9": 57,
+    "play": 64,
+    "pause": 65,
+    "stop": 66,
+    "record": 67,
+    "fastforward": 69,
+    "rewind": 71,
+    "boxoffice": 240,
+    "sky": 241,
+}
+VALID_KEYS = list(_KEY_MAP.keys())
+
+
 class RemoteControl:
     """RemoteControl class of sky_remote."""
 
@@ -25,49 +71,7 @@ class RemoteControl:
         """Set up the connection variables and keymap."""
         self.host = host
         self.port = port
-        self.key_map = {
-            "power": 0,
-            "select": 1,
-            "backup": 2,
-            "dismiss": 2,
-            "channelup": 6,
-            "channeldown": 7,
-            "interactive": 8,
-            "sidebar": 8,
-            "help": 9,
-            "services": 10,
-            "search": 10,
-            "tvguide": 11,
-            "home": 11,
-            "i": 14,
-            "text": 15,
-            "up": 16,
-            "down": 17,
-            "left": 18,
-            "right": 19,
-            "red": 32,
-            "green": 33,
-            "yellow": 34,
-            "blue": 35,
-            "0": 48,
-            "1": 49,
-            "2": 50,
-            "3": 51,
-            "4": 52,
-            "5": 53,
-            "6": 54,
-            "7": 55,
-            "8": 56,
-            "9": 57,
-            "play": 64,
-            "pause": 65,
-            "stop": 66,
-            "record": 67,
-            "fastforward": 69,
-            "rewind": 71,
-            "boxoffice": 240,
-            "sky": 241,
-        }
+        self.key_map = _KEY_MAP
 
     async def check_connectable(self, timeout=3) -> bool:
         """Return True if device is probably a Sky box."""
@@ -76,6 +80,8 @@ class RemoteControl:
             reader, writer = await asyncio.wait_for(connection_future, timeout)
         except asyncio.TimeoutError as e:
             raise ConnectionTimeoutError from e
+        except OSError as e:
+            raise SkyBoxConnectionError from e
         try:
             data = await asyncio.wait_for(reader.read(3), timeout)
         except asyncio.TimeoutError as e:
